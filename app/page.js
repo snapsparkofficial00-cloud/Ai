5e42e28041743e7a8007799c1e6472d6b54d0384
snapsparkofficial00-cloud <snapsparkofficial00@gmail.com>
2026-05-25 23:36:50 +0530

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <main style={{
+      height: "100vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      fontSize: "40px",
+      fontWeight: "bold"
+    }}>
+      Hello Snap Spark 🚀
+    </main>
+  );
+}
