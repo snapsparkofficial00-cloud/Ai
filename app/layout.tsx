56db57e8f12aa1ba4b0f2491d5ad197fe8cda8f4
snapsparkofficial00-cloud <snapsparkofficial00@gmail.com>
2026-05-26 17:13:39 +0530

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+import "./globals.css";
+import Sidebar from "./components/Sidebar";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <Sidebar>
+          {children}
+        </Sidebar>
+      </body>
+    </html>
+  );
+}
