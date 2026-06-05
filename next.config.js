a9186aaa818953fb7640110351d9c10abcccf717
snapsparkofficial00-cloud <snapsparkofficial00@gmail.com>
2026-06-05 16:23:51 +0530

    next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
-
-// Add this block to your existing next.config.js
-// If next.config.js doesn't exist, create it with this content:
-
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   serverComponentsExternalPackages: [
     'puppeteer',
